24f5bf36100130c4872c71b20d967e7eb8c3842e
despresc <christian.j.j.despres@gmail.com>
2021-08-14 15:51:25 -0400

    Fix parsing bugs, finish the naive interpreter

---
+++ example/addition.bf
@@ -0,0 +1,5 @@
+[ Adapted from the Wikipedia article again
+
+  Reads two numbers from the input and outputs their sum
+]
+,>,<[->+<]>.
--- src/Hasbrain.hs
+++ src/Hasbrain.hs
@@ -1,15 +1,20 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Hasbrain where
 
 import Control.Applicative ((<|>))
+import Control.Monad.RWS.Strict (RWS)
+import qualified Control.Monad.RWS.Strict as RWS
 import Data.Functor (($>))
 import qualified Data.Set as S
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.Traversable (for)
 import Data.Void
-import qualified Text.Megaparsec as MP
 import Data.Word (Word8)
+import qualified Text.Megaparsec as MP
 
 {-
 The brainfuck instruction set:
@@ -22,6 +27,9 @@ The brainfuck instruction set:
 , | accept one byte of input and store in the data pointer
 [ | if the data pointer value is zero, jump to the instruction after the matching ]
 ] | if the data pointer value is nonzero, jump to the instruction after the matching [
+
+All other characters are uninterpreted. We treat runs of such characters as
+comments here.
 -}
 
 data Instr
@@ -35,6 +43,20 @@ data Instr
   | JumpFromRight
   | Comment !Text
   deriving (Eq, Ord, Show)
+
+renderInstr :: Instr -> Text
+renderInstr PointerRight = ">"
+renderInstr PointerLeft = "<"
+renderInstr Increment = "+"
+renderInstr Decrement = "-"
+renderInstr Output = "."
+renderInstr Input = ","
+renderInstr JumpFromLeft = "["
+renderInstr JumpFromRight = "]"
+renderInstr (Comment t) = t
+
+renderInstrs :: [Instr] -> Text
+renderInstrs = foldMap renderInstr
 
 {-
 A brainfuck program is a sequence of instructions and other (ignored)
@@ -69,11 +91,13 @@ pInstr =
     <|> "+" $> Increment
     <|> "-" $> Decrement
     <|> "." $> Output
+    <|> "," $> Input
     <|> comment
     <|> bracket
   where
-    -- all the other source characters will have been recognized by now
-    comment = fmap Comment $ MP.takeWhile1P Nothing $ \c -> c /= '[' && c /= ']'
+    comment = fmap Comment $
+      MP.takeWhile1P Nothing $
+        \c -> c `notElem` ['>', '<', '+', '-', '.', ',', '[', ']']
     -- at this point in the chain the only possible characters left are the
     -- brackets
     bracket = do
@@ -105,9 +129,183 @@ parseProgram name =
   either (Left . T.pack . MP.errorBundlePretty) Right
     . MP.runParser pAllInstrs (T.unpack name)
 
--- | A tape with focus for the cells and instructions
-data Tape a = Tape [a] !a [a]
+stripComments :: [Instr] -> [Instr]
+stripComments = filter $ \x -> case x of
+  Comment _ -> False
+  _ -> True
 
--- | We start with an infinite tape for the cells, all initialized to zero
-initCellTape :: Tape Word8
-initCellTape = Tape (repeat 0) 0 (repeat 0)
+-- | An infinite stream of 'Word8' values
+data WordStream = WordStream
+  { streamHead :: !Word8,
+    streamTail :: WordStream
+  }
+
+constStream :: Word8 -> WordStream
+constStream w = WordStream w $ constStream w
+
+constStreamWithPrefix :: [Word8] -> Word8 -> WordStream
+constStreamWithPrefix (x : xs) y = WordStream x $ constStreamWithPrefix xs y
+constStreamWithPrefix [] y = constStream y
+
+-- | The instruction tape, with the head of the second list being the current
+-- instruction
+data InstrTape = InstrTape [Instr] [Instr]
+  deriving (Eq, Ord, Show)
+
+-- | The data tape
+data DataTape = DataTape WordStream !Word8 WordStream
+
+-- | The data tape starts with all cells initialized to zero
+initCellTape :: DataTape
+initCellTape = DataTape zeroes 0 zeroes
+  where
+    zeroes = constStream 0
+
+initInstrTape :: [Instr] -> InstrTape
+initInstrTape = InstrTape []
+
+popInstrRight :: InstrTape -> Maybe (Instr, InstrTape)
+popInstrRight (InstrTape ls (r : rs)) = Just (r, InstrTape ls rs)
+popInstrRight _ = Nothing
+
+popInstrLeft :: InstrTape -> Maybe (Instr, InstrTape)
+popInstrLeft (InstrTape (l : ls) rs) = Just (l, InstrTape ls rs)
+popInstrLeft _ = Nothing
+
+putInstrRight :: Instr -> InstrTape -> InstrTape
+putInstrRight i (InstrTape ls rs) = InstrTape ls (i : rs)
+
+putInstrLeft :: Instr -> InstrTape -> InstrTape
+putInstrLeft i (InstrTape ls rs) = InstrTape (i : ls) rs
+
+-- | Move right along the tape until just after an unmatched 'JumpFromRight' is
+-- encountered, or until the end of the tape is reached
+seekRight :: InstrTape -> InstrTape
+seekRight = go (0 :: Int)
+  where
+    go !n it = case moveR it of
+      Just (JumpFromLeft, it') -> go (n + 1) it'
+      Just (JumpFromRight, it')
+        | n == 0 -> it'
+        | otherwise -> go (n - 1) it'
+      Just (_, it') -> go n it'
+      Nothing -> it
+    moveR it = case popInstrRight it of
+      Just (x, it') -> Just (x, putInstrLeft x it')
+      Nothing -> Nothing
+
+-- | Move left along the tape until just before an unmatched 'JumpFromLeft' is
+-- encountered, or until the beginning of the tape is reached
+seekLeft :: InstrTape -> InstrTape
+seekLeft = go (0 :: Int)
+  where
+    -- Necessarily implemented a little differently from seekRight, though a
+    -- slightly more complicated function could work for both
+    go !n it = case moveL it of
+      Just (JumpFromRight, it') -> go (n + 1) it'
+      Just (JumpFromLeft, it')
+        | n == 0 -> it -- NOT it'
+        | otherwise -> go (n - 1) it'
+      Just (_, it') -> go n it'
+      Nothing -> it
+    moveL it = case popInstrLeft it of
+      Just (x, it') -> Just (x, putInstrRight x it')
+      Nothing -> Nothing
+
+-- | Move the data pointer right
+dataRight :: DataTape -> DataTape
+dataRight (DataTape l c (WordStream n w)) = DataTape (WordStream c l) n w
+
+-- | Move the data pointer left
+dataLeft :: DataTape -> DataTape
+dataLeft (DataTape (WordStream n w) c r) = DataTape w n (WordStream c r)
+
+-- | Increment the current cell
+dataInc :: DataTape -> DataTape
+dataInc (DataTape l c r) = DataTape l (c + 1) r
+
+-- | Decrement the current cell
+dataDec :: DataTape -> DataTape
+dataDec (DataTape l c r) = DataTape l (c - 1) r
+
+-- | Read the value of the current cell
+readData :: DataTape -> Word8
+readData (DataTape _ c _) = c
+
+-- | Set the value of the current cell
+setData :: Word8 -> DataTape -> DataTape
+setData w (DataTape l _ r) = DataTape l w r
+
+class Monad m => MonadInteract m where
+  getByte :: m Word8
+  putByte :: Word8 -> m ()
+
+data BrainState = BrainState
+  { brainInstrs :: InstrTape,
+    brainData :: DataTape
+  }
+
+initBrainState :: [Instr] -> BrainState
+initBrainState is = BrainState (initInstrTape is) initCellTape
+
+stepState :: MonadInteract m => BrainState -> m (Maybe BrainState)
+stepState bs = for (popInstrRight $ brainInstrs bs) $ \(ci, bis) ->
+  let bsL = bs {brainInstrs = putInstrLeft ci bis}
+   in case ci of
+        PointerRight -> pure $ bsL {brainData = dataRight $ brainData bs}
+        PointerLeft -> pure $ bsL {brainData = dataLeft $ brainData bs}
+        Increment -> pure $ bsL {brainData = dataInc $ brainData bs}
+        Decrement -> pure $ bsL {brainData = dataDec $ brainData bs}
+        Output -> do
+          putByte $ readData $ brainData bs
+          pure bsL
+        Input -> do
+          w <- getByte
+          pure $ bsL {brainData = setData w $ brainData bs}
+        JumpFromLeft
+          | readData (brainData bs) == 0 ->
+            pure $ bs {brainInstrs = seekRight $ putInstrLeft ci bis}
+          | otherwise ->
+            pure bsL
+        JumpFromRight
+          | readData (brainData bs) /= 0 ->
+            pure $ bs {brainInstrs = seekLeft $ putInstrRight ci bis}
+          | otherwise ->
+            pure bsL
+        Comment _ -> pure bsL
+
+-- | Run a 'BrainState' until it halts
+runBrain :: MonadInteract m => BrainState -> m BrainState
+runBrain b = do
+  mb <- stepState b
+  case mb of
+    Just b' -> runBrain b'
+    Nothing -> pure b
+
+newtype DL a = DL {unDL :: [a] -> [a]}
+
+instance Semigroup (DL a) where
+  DL x <> DL y = DL (x . y)
+
+instance Monoid (DL a) where
+  mempty = DL id
+
+singletonDL :: a -> DL a
+singletonDL = DL . (:)
+
+-- | A pure simulation of input and output
+newtype PureInteract a = PureInteract
+  {unPureInteract :: RWS () (DL Word8) WordStream a}
+  deriving (Functor, Applicative, Monad)
+
+instance MonadInteract PureInteract where
+  getByte = PureInteract $ do
+    WordStream n w <- RWS.get
+    RWS.put w
+    pure n
+  putByte = PureInteract . RWS.tell . singletonDL
+
+runPureInteract :: PureInteract a -> WordStream -> ([Word8], a)
+runPureInteract act = go . RWS.runRWS (unPureInteract act) ()
+  where
+    go (a, _, dl) = (unDL dl [], a)
